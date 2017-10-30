3c5cd36bba55f85cfebc92eb5341d7b6c8b4f79b
p-lucero <p-lucero@users.noreply.github.com>
2017-10-30 13:18:16 -0600

    Add minimalist UI deployment script
    See comments in the file for more information. Already present on the server,
    but I've added it here for thoroughness' sake.

---
+++ deploy_ui.sh
@@ -0,0 +1,7 @@
+# To invoke this script as a one-liner through SSH:
+# ssh -i ~/.ssh/<YOUR FIRST NAME>.pem <YOUR FIRST NAME>@ec2-18-216-3-210.us-east-2.compute.amazonaws.com -t 'echo <YOUR SERVER PASSWORD> | sudo -Su ubuntu /srv/SharedSpacesDeployment/deploy_ui.sh'
+cd /srv/SharedSpacesDeployment/github-building/react-web
+git pull
+npm run build
+rm -rf /srv/SharedSpacesDeployment/build
+cp -r /srv/SharedSpacesDeployment/github-building/react-web/build /srv/SharedSpacesDeployment/client
