08ec0783bb37e270dfcc8db1e972d914a13e665e
p-lucero <p-lucero@users.noreply.github.com>
2017-11-01 10:47:26 -0600

    Correct typo from blindly copying

--- server-side/server.js
+++ server-side/server.js
@@ -21,7 +21,7 @@ con.connect(function(err){
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var routes = require('./api/routes/todoListRoutes'); //importing route
+var routes = require('./api/routes/serverRoutes'); //importing route
 routes(app); //register the route
 
 app.listen(port);
