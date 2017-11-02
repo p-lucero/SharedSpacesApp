6ec8d150cd65da7ea2e6510c5116b2e0a536d84d
p-lucero <p-lucero@users.noreply.github.com>
2017-11-02 17:58:52 -0600

    Add tentative backend deployment script
    This assumes that we don't need to restart any servers since Nodemon should be
    keeping watch for us when files are changed. Going to set this up soon so that
    all of the Express code I've written is running on the deployment environment
    and I can actually test and prototype things for real rather than with toy
    Python scripts.

---
+++ deploy_backend.sh
@@ -0,0 +1,5 @@
+# To invoke this script as a one-liner through SSH:
+# ssh -i ~/.ssh/<YOUR FIRST NAME>.pem <YOUR FIRST NAME>@ec2-18-216-3-210.us-east-2.compute.amazonaws.com -t 'echo <YOUR SERVER PASSWORD> | sudo -Su ubuntu /srv/SharedSpacesDeployment/deploy_backend.sh'
+cd /srv/SharedSpacesDeployment/github-building/server-side
+git pull
+cp -r /srv/SharedSpacesDeployment/github-building/server-side /srv/SharedSpacesDeployment/server
