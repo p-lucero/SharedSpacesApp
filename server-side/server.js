11daf59e49a3f686b1a6e60668726aebe00847df
p-lucero <p-lucero@users.noreply.github.com>
2017-11-02 14:32:44 -0600

    Add middleware to intercept invalid requests
    Any request that's not specified in routes should be 404'd by this bit of
    middleware. As yet untested; may need to have a different precedence in the
    file compared to some of the other statements. Putting this up for now.

--- server-side/server.js
+++ server-side/server.js
@@ -21,6 +21,10 @@ con.connect(function(err){
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(function(req, res) {
+  res.status(404).send({url: req.originalUrl + ' not found'})
+});
+
 var routes = require('./api/routes/serverRoutes'); //importing route
 routes(app); //register the route
 
