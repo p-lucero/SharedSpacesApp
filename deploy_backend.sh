fbf5a277278d5f0f334151b8ea64bb19dbe7416f
p-lucero <p-lucero@users.noreply.github.com>
2017-11-02 18:13:01 -0600

    Fix error with server deploy script

--- deploy_backend.sh
+++ deploy_backend.sh
@@ -2,4 +2,4 @@
 # ssh -i ~/.ssh/<YOUR FIRST NAME>.pem <YOUR FIRST NAME>@ec2-18-216-3-210.us-east-2.compute.amazonaws.com -t 'echo <YOUR SERVER PASSWORD> | sudo -Su ubuntu /srv/SharedSpacesDeployment/deploy_backend.sh'
 cd /srv/SharedSpacesDeployment/github-building/server-side
 git pull
-cp -r /srv/SharedSpacesDeployment/github-building/server-side /srv/SharedSpacesDeployment/server
+cp -a /srv/SharedSpacesDeployment/github-building/server-side/. /srv/SharedSpacesDeployment/server
