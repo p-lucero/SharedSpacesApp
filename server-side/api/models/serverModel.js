1de81866424da8b69a32b5d0b12de671e27a3129
p-lucero <p-lucero@users.noreply.github.com>
2017-11-06 22:52:16 -0700

    Remove unnecessary file
    models folder to be populated later with parsing and authentication models.

--- server-side/api/models/serverModel.js
+++
@@ -1,4 +0,0 @@
-// This file should contain a description of the database schema in a more formal manner.
-// In the example code that I'm working with, this is done assuming we're using MongoDB, which is not correct.
-// TODO TODO TODO TODO TODO
-// Since we're using mysql, this is most likely an obsolete file.
