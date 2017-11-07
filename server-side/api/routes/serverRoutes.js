9feac2c1ab78514020d703e91e9fa43f4db413d0
p-lucero <p-lucero@users.noreply.github.com>
2017-11-06 22:41:50 -0700

    Restructure API to avoid server file bloat

--- server-side/api/routes/serverRoutes.js
+++ server-side/api/routes/serverRoutes.js
@@ -1,78 +1,81 @@
 'use strict';
 module.exports = function(app) {
-	var controller = require('../controllers/serverController.js');
+	var getController = require('../controllers/getController.js');
+	var putController = require('../controllers/putController.js');
+	var postController = require('../controllers/postController.js');
+	var deleteController = require('../controllers/deleteController.js');
 
 	app.route('/api/groups') // deals with all groups or groups in the making
-		.post(controller.create_new_group)
+		.post(postController.create_new_group)
 
 	app.route('/api/groups/:groupId')
-		.get(controller.get_group_info)
-		.put(controller.update_group_info)
-		.delete(controller.delete_group)
+		.get(getController.get_group_info)
+		.put(putController.update_group_info)
+		.delete(deleteController.delete_group)
 
 	app.route('/api/users')
-		.post(controller.create_new_user)
+		.post(postController.create_new_user)
 
 	app.route('/api/users/:userId')
-		.get(controller.get_user_info)
-		.put(controller.update_user_info)
-		.delete(controller.delete_user)
+		.get(getController.get_user_info)
+		.put(putController.update_user_info)
+		.delete(deleteController.delete_user)
 
 	app.route('/api/groupDebts/:groupId')
-		.get(controller.get_group_debt_list)
-		.post(controller.create_group_debt)
+		.get(getController.get_group_debt_list)
+		.post(postController.create_group_debt)
 
 	app.route('/api/groupDebts/:groupId/:debtId')
-		.get(controller.get_group_debt_info)
-		.put(controller.update_group_debt)
-		.delete(controller.delete_group_debt)
+		.get(getController.get_group_debt_info)
+		.put(putController.update_group_debt)
+		.delete(deleteController.delete_group_debt)
 
 	app.route('/api/personalDebts')
-		.post(controller.create_new_personal_debt)
+		.post(postController.create_new_personal_debt)
 
 	app.route('/api/personalDebts/:userId')
-		.get(controller.list_personal_debts)
+		.get(getController.list_personal_debts)
 
 	app.route('/api/personalDebts/:userId/:debtId')
-		.get(controller.get_personal_debt_info)
-		.put(controller.update_personal_debt)
-		.delete(controller.delete_personal_debt)
+		.get(getController.get_personal_debt_info)
+		.put(putController.update_personal_debt)
+		.delete(deleteController.delete_personal_debt)
 
 	app.route('/api/groceries/:groupId')
-		.get(controller.get_grocery_list)
-		.put(controller.update_grocery_list)
-		.post(controller.create_new_grocery_item)
-		.delete(controller.delete_grocery_list)
+		.get(getController.get_grocery_list)
+		.put(putController.update_grocery_list)
+		.post(postController.create_new_grocery_item)
+		.delete(deleteController.delete_grocery_list)
 
 	app.route('/api/groceries/:groupId/:groceryId')
-		.get(controller.get_grocery_item)
-		.put(controller.update_grocery_item)
-		.delete(controller.delete_grocery_item)
+		.get(getController.get_grocery_item)
+		.put(putController.update_grocery_item)
+		.delete(deleteController.delete_grocery_item)
 
 	app.route('/api/chores/:groupId')
-		.get(controller.get_chores_list)
-		.put(controller.update_chores_list)
-		.post(controller.create_new_chore)
-		.delete(controller.delete_chores_list)
+		.get(getController.get_chores_list)
+		.put(putController.update_chores_list)
+		.post(postController.create_new_chore)
+		.delete(deleteController.delete_chores_list)
 
 	app.route('/api/chores/:groupId/:choreId')
-		.get(controller.get_chore_info)
-		.put(controller.update_chore_item)
-		.delete(controller.delete_chore_item)
+		.get(getController.get_chore_info)
+		.put(putController.update_chore_item)
+		.delete(deleteController.delete_chore_item)
 
 	app.route('/api/rent/:groupId')
-		.get(controller.get_rent_info)
-		.put(controller.update_rent_info)
-		.post(controller.create_new_rent_item)
+		.get(getController.get_rent_info)
+		.put(putController.update_rent_info)
+		.post(postController.create_new_rent_item)
 
 	// Enumerate all of the routes that should be taken by different requests.
 	// The general form is:
 	/*
 		app.route('some path the client will visit to submit a request')
-			.get(controller.the appropriate route)
-			.put(controller.the appropriate route)
-			.post(controller.the appropriate route)
-			.delete(controller.the appropriate route)
+			.get(getController.the appropriate route)
+			.put(putController.the appropriate route)
+			.post(postController.the appropriate route)
+			.delete(deleteController.the appropriate route)
 	*/
 	// A given method that is not supported for a certain task should be omitted here.
 	// For example, DELETE is not supported on users; it's not possible to use the API to delete all user accounts.
