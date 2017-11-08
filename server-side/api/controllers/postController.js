7647cda515f7924b9545348adb1e2cf0b2ef808b
p-lucero <p-lucero@users.noreply.github.com>
2017-11-08 11:47:12 -0700

    Catch a badly named controllerjs file

--- server-side/api/controllers/postController.js
+++ server-side/api/controllers/postController.js
@@ -2,6 +2,24 @@
 
 var mysql = require('mysql');
 
+exports.create_new_group = function(request, result){
+	// DUMMY FUNCTION
+	var parsed_request = "use deployment;"; // Dummy request for now; this should be whatever we get out of the parsing code to be written above!
+	var authenticated = true; // Dummy boolean for now; this should be whatever the above authentication code returned
+	if (authenticated) {
+		global.con.query(parsed_request, function(err, task) {
+			if (err) {
+				result.send(err); // This must be interpreted by the client. Make a way to do this in the UI!
+			}
+			else
+				result.json(task); // Return the results of the SQL query to the client to be interpreted and pretty-printed by the React UI
+		});
+	}
+	else {
+		result.status(403).send({url: request.originalUrl + " forbidden"})
+	}
+};
+
 /*
 	exports.FUNCTIONNAME = function(request, result) {
 		// Parse the request into a valid SQL query with some common parser code from models
@@ -33,3 +51,10 @@ var mysql = require('mysql');
 		}
 	};
 */
+
+// Fire away at this server with the following Python code to beta test things:
+// >>> import requests
+// >>> r = requests.get(<address, probably localhost or ec2>, data=<dictionary containing lots of fields>)
+// >>> r.content
+
+// If this is running on the server, you can also test the routing by accessing SERVERNAME/api/* from outside
