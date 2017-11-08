fea2106cabfa690e2ddb6420d8ae020fe279580a
p-lucero <p-lucero@users.noreply.github.com>
2017-11-08 11:45:25 -0700

    Unzip and set up Brittany's Android Studio code

---
+++ android-studio/app/src/main/java/com/sharedspaces/sharedspaces/MessageFragment.java
@@ -0,0 +1,21 @@
+package com.sharedspaces.sharedspaces;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+//TODO: implement chat system and info on who is in group
+
+public class MessageFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.message_fragment, container, false);
+    }
+
+
+}
