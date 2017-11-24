1eda98a778cb6714b3aad4304a4b8109dcec83b9
p-lucero <p-lucero@users.noreply.github.com>
2017-11-24 15:27:07 -0700

    Add first draft of fleshed-out postController
    In all likelihood, most of the stuff here is probably broken. However, it's at
    the very least helped me figure out how to structure commonModel better, for
    what that's worth.

--- server-side/api/controllers/postController.js
+++ server-side/api/controllers/postController.js
@@ -6,56 +6,76 @@ var common = require('../models/commonModel.js');
 exports.create_new_group = function(request, result){
 	attributes = ["groupName"]
 	placeholders = ["groupName"]
-	skeleton = "INSERT INTO groups (group_name) VALUES ('groupName')"
+	skeleton = "INSERT INTO groups (group_name) VALUES (groupName)"
 	specificAuth = true
-	common.perform_query(attributes, placeholders, skeleton, specificAuth, request, result)
+	if (!common.perform_query(attributes, placeholders, skeleton, specificAuth, request, result)) {
+		common.perform_query(attributes, placeholders, "SELECT id FROM groups WHERE group_name=groupName;", specificAuth, request, result)
+	}
 };
 
 exports.create_new_user = function(request, result) {
-	attributes = ["first_name, last_name, email, password"]
-	placeholders = [""]
+	attributes = ["first", "last", "_email", "pw", "phoneNumber"]
+	placeholders = ["first", "last", "_email", "pw", "phoneNumber", "facebook", "twitter", "insta", "groupID"]
 	skeleton = "INSERT INTO user_accounts (first_name, last_name, email, password, phone_number, facebook_profile, twitter_handle, instagram, group_id)\
-	VALUES (first, last, _email, pw, phoneNumber, facebook, twitter, insta, groupID)"
+	VALUES (first, last, _email, pw, phoneNumber, facebook, twitter, insta, groupID);"
+	request.body._email = request.body.email // deal with name shadowing
+	request.body.pw = request.body.password
+	request.body.insta = request.body.instagram
 	specificAuth = true // this doesn't need to be fixed; anyone should be able to create user accounts without any specific authentication
-	common.perform_query(attributes, placeholders, skeleton, specificAuth, request, result)
+	if (!common.perform_query(attributes, placeholders, skeleton, specificAuth, request, result)) {
+		common.perform_query(["_email"], ["_email"], "SELECT id from user_accounts WHERE email=_email;", specificAuth, request, result)
+	}
 };
 
 exports.create_group_debt = function(request, result) {
-	attributes = []
-	placeholders = []
-	skeleton = ""
+	attributes = ["debtType", "_amount"]
+	placeholders = ["debtType", "_amount", "groupId"]
+	skeleton = "INSERT INTO group_debt(debt_type, amount, group_id) VALUES (debtType, _amount, groupId);"
+	request.body._amount = request.body.amount // deal with name shadowing
 	specificAuth = true
-	common.perform_query(attributes, placeholders, skeleton, specificAuth, request, result)
+	if (!common.perform_query(attributes, placeholders, skeleton, specificAuth, request, result)) {
+		result.status(200).send(' ') // anything that's not an error should be interpreted as good 
+	}
 };
 
 exports.create_new_personal_debt = function(request, result) {
-	attributes = []
-	placeholders = []
-	skeleton = ""
+	attributes = ["lender", "borrower"]
+	placeholders = ["_amount", "lender", "borrower"]
+	skeleton = "INSERT INTO personal_debts (amount, lender_id, borrower_id) VALUES (_amount, lender, borrower);"
 	specificAuth = true
-	common.perform_query(attributes, placeholders, skeleton, specificAuth, request, result)
+	if (!common.perform_query(attributes, placeholders, skeleton, specificAuth, request, result)){
+		result.status(200).send(' ')
+	}
 };
 
 exports.create_new_grocery_item = function(request, result) {
-	attributes = []
-	placeholders = []
-	skeleton = ""
+	attributes = ["amount", "userID"]
+	placeholders = ["amount", "userID", "groupId"]
+	skeleton = "INSERT INTO groceries (amount_due, paid_status, user_id, group_id) VALUES (amount, false, userID, groupId);"
 	specificAuth = true
-	common.perform_query(attributes, placeholders, skeleton, specificAuth, request, result)
+	if (!common.perform_query(attributes, placeholders, skeleton, specificAuth, request, result)){
+		result.status(200).send(' ')
+	}
 };
 
 exports.create_new_chore = function(request, result) {
-	attributes = []
-	placeholders = []
-	skeleton = ""
+	attributes = ["_chore", "duedate", "userID"]
+	placeholders = ["_chore", "duedate", "userID", "groupId"]
+	skeleton = "INSERT INTO chores (chore, due_date, chore_complete, user_id, group_id) VALUES (_chore, duedate, false, userID, groupId);"
+	request.body._chore = request.body.chore // deal with name shadowing
+	request.body.duedate = request.body.due_date
 	specificAuth = true
-	common.perform_query(attributes, placeholders, skeleton, specificAuth, request, result)
+	if (!common.perform_query(attributes, placeholders, skeleton, specificAuth, request, result)){
+		result.status(200).send(' ')
+	}
 };
 
 exports.create_new_rent_item = function(request, result) {
-	attributes = []
-	placeholders = []
-	skeleton = ""
+	attributes = ["amount", "userID"]
+	placeholders = ["amount", "userID", "groupId"]
+	skeleton = "INSERT INTO rent (rent_amount, rent_paid, user_id, group_id) VALUES (amount, false, userID, groupId)"
 	specificAuth = true
-	common.perform_query(attributes, placeholders, skeleton, specificAuth, request, result)
+	if (!common.perform_query(attributes, placeholders, skeleton, specificAuth, request, result)){
+		result.status(200).send(' ')
+	}
 };
