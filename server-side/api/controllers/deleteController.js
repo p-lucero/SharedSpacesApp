d9c11fa2bf360effc956fb0e31b2f12d44dec587
p-lucero <p-lucero@users.noreply.github.com>
2017-11-24 15:21:30 -0700

    On second thought, it should probably be this way

--- server-side/api/controllers/deleteController.js
+++ server-side/api/controllers/deleteController.js
@@ -5,64 +5,64 @@ var common = require('../models/commonModel.js');
 
 exports.delete_group = function(request, result) {
 	attributes = []
-	placeholders = ["groupID"]
-	skeleton = "DELETE FROM groups WHERE id=groupID;"
+	placeholders = ["groupId"]
+	skeleton = "DELETE FROM groups WHERE id=groupId;"
 	specificAuth = true
 	common.perform_query(attributes, placeholders, skeleton, specificAuth, request, result)
 };
 
 exports.delete_user = function(request, result) {
 	attributes = [] 
-	placeholders = ["userID"] 
-	skeleton = "DELETE FROM users WHERE id=userID;"
+	placeholders = ["userId"] 
+	skeleton = "DELETE FROM users WHERE id=userId;"
 	specificAuth = true
 	common.perform_query(attributes, placeholders, skeleton, specificAuth, request, result)
 };
 
 exports.delete_group_debt = function(request, result) {
 	attributes = [] 
-	placeholders = ["debtID"] 
-	skeleton = "DELETE FROM group_debt WHERE id=debtID;"
+	placeholders = ["debtId"] 
+	skeleton = "DELETE FROM group_debt WHERE id=debtId;"
 	specificAuth = true
 	common.perform_query(attributes, placeholders, skeleton, specificAuth, request, result)
 };
 
 exports.delete_personal_debt = function(request, result) {
 	attributes = []
-	placeholders = ["debtID"]
-	skeleton = "DELETE FROM personal_debts WHERE id=debtID;"
+	placeholders = ["debtId"]
+	skeleton = "DELETE FROM personal_debts WHERE id=debtId;"
 	specificAuth = true
 	common.perform_query(attributes, placeholders, skeleton, specificAuth, request, result)
 };
 
 exports.delete_grocery_list = function(request, result) {
 	attributes = []
-	placeholders = ["groupID"]
-	skeleton = "DELETE FROM groceries WHERE group_id=groupID;"
+	placeholders = ["groupId"]
+	skeleton = "DELETE FROM groceries WHERE group_id=groupId;"
 	specificAuth = true
 	common.perform_query(attributes, placeholders, skeleton, specificAuth, request, result)
 };
 
 exports.delete_grocery_item = function(request, result) {
 	attributes = []
-	placeholders = ["GgoceryID"]
-	skeleton = "DELETE FROM groceries WHERE id=groceryID"
+	placeholders = ["groceryId"]
+	skeleton = "DELETE FROM groceries WHERE id=groceryId"
 	specificAuth = true
 	common.perform_query(attributes, placeholders, skeleton, specificAuth, request, result)
 };
 
 exports.delete_chores_list = function(request, result) {
 	attributes = []
-	placeholders = ["GgoupID"]
-	skeleton = "DELETE FROM chores WHERE group_id=groupID"
+	placeholders = ["groupId"]
+	skeleton = "DELETE FROM chores WHERE group_id=groupId"
 	specificAuth = true
 	common.perform_query(attributes, placeholders, skeleton, specificAuth, request, result)
 };
 
 exports.delete_chore_item = function(request, result) {
 	attributes = []
-	placeholders = ["CcoreID"]
-	skeleton = "DELETE FROM chores WHERE id=choreID"
+	placeholders = ["choreId"]
+	skeleton = "DELETE FROM chores WHERE id=choreId"
 	specificAuth = true
 	common.perform_query(attributes, placeholders, skeleton, specificAuth, request, result)
 };
