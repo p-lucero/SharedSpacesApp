0a8d99114531083db4eae140a7b6b34c1455ac2c
p-lucero <p-lucero@users.noreply.github.com>
2017-11-25 16:27:32 -0700

    Move authentication functions to the right place
    I've been coding for too long. Just clicking on things at random in my sidebar
    and typing letters that seem to make sense at this point. Aaaaaugh. Taking a
    break now.

--- server-side/api/controllers/authenticationController.js
+++ server-side/api/controllers/authenticationController.js
@@ -1,3 +1,11 @@
 'use strict';
 
 // some sort of code to cache credentials goes here
+
+exports.login = function(){
+	// dummy
+}
+
+exports.logout = function(){
+	// dummy
+}
--- server-side/api/models/authenticationModel.js
+++ server-side/api/models/authenticationModel.js
@@ -1,11 +1,3 @@
 'use strict';
 
 // some sort of code to cache credentials goes here
-
-exports.login = function(){
-	// dummy
-}
-
-exports.logout = function(){
-	// dummy
-}
