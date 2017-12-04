2a16b0ba48d99536f10814cd6428df48b3dbde86
p-lucero <p-lucero@users.noreply.github.com>
2017-12-03 22:05:09 -0700

    Add put tester
    The last of the totally untested testers. Hopefully everything here is at least
    moderately working. To run `npm test` on the server momentarily and update with
    how bad the results are in a screenshot for TESTING.md.

--- server-side/test/putTester.js
+++ server-side/test/putTester.js
@@ -4,9 +4,114 @@ process.env.PORT = 3002;
 var chai  = require('chai');
 var chaiHttp = require('chai-http');
 var server = require('../server');
+var webServer = server.server
+var app = server.app
+var execsql = require('execsql');
 var expect = chai.expect;
+let retcode
 
 chai.use(chaiHttp);
+
+var dbConfig = {
+	host: 'localhost',
+	user: 'root',
+	insecureAuth: true
+}
+var selectDB = `use ${process.env.DATABASE};`
+var sqlFile = __dirname + '/../testing_db_data.sql'
+
+var endpoints = [{
+	name: 'Update group information',
+	uri: '/api/groups/1',
+	badQuery: {
+		groupName: 'Absolutely Not Shared Spaces',
+		groundRules: 'Don\'t be that guy'
+	},
+	goodQuery: {
+		groupName: 'Totally Shared Spaces',
+		groundRules: 'Be that guy',
+		users: [1,2,3,4]
+	}
+},
+{
+	name: 'Update user information',
+	uri: '/api/users/1',
+	badQuery: {
+		first:'foo'
+	},
+	goodQuery: {
+		first:'Kyle',
+		last:'McDevitt',
+		email:'kyle@ymail.com',
+		password:'wordpass',
+		phoneNumber:1,
+		facebook:'kmcd',
+		twitter:'@kmcd',
+		instagram:'@kmcd',
+		groupID:1
+	}
+},
+{
+	name: 'Update group debt',
+	uri: '/api/groupDebts/1/1',
+	badQuery: {
+		debtType: 'Painted the town green'
+	},
+	goodQuery: {
+		debtType: 'Painted the town green',
+		amount: 8
+	}
+},
+{
+	name: 'Update personal debt',
+	uri: '/api/personalDebts/1/1',
+	badQuery: {
+		amount: 2
+	},
+	goodQuery: {
+		amount: 2,
+		lenderID: 1,
+		borrowerID: 4
+	}
+},
+{
+	name: 'Update grocery item',
+	uri: '/api/groceries/1/1',
+	badQuery: {
+		amount: 12
+	},
+	goodQuery: {
+		amount: 14,
+		paid: true,
+		userID: 3
+	}
+},
+{
+	name: 'Update chore item',
+	uri: '/api/chores/1/1',
+	badQuery: {
+		complete:false
+	},
+	goodQuery: {
+		chore: 'Leave the apartment forever and never come back',
+		duedate: '08-30-2017',
+		complete: false,
+		userID: 2
+	}
+},
+{
+	name: 'Update rent information',
+	uri: '/api/rent/1',
+	badQuery: {
+		amount: 1000000000
+	},
+	goodQuery: {
+		amount: 1000000000,
+		paid: true,
+		userID: 1
+	}
+}
+]
 
 // http://chaijs.com/api/bdd/ contains the documentation for how things work here
 
@@ -28,131 +133,106 @@ var badUser = {
 	phone_number: 9999999999
 }
 
+var lUser = {
+	id: 5,
+	first_name: 'Lame',
+	last_name: 'Loser',
+	email: 'lameloser@gmail.com',
+	password: 'password',
+	phone_number: 3333333333
+}
+
 describe('The put endpoints', function(){
 	before(function(done){
 		execsql.config(dbConfig).exec(selectDB).execFile(sqlFile, function(err, results){
 			if (err) throw err;
 			console.log(results);
-			done()
-		}).end();
+			let request = dummyUser
+			request.stayLoggedIn = true
+			chai.request(server)
+				.post('/api/login')
+				.send(request)
+				.end((err, res) => {
+					expect(res.status).to.equal(200);
+					expect(res.body).to.have.property('success')
+					expect(res.body).to.have.property('token')
+					expect(res.body).to.have.property('user_id')
+					dummyUser.token = res.body.token
+					let otherRequest = lUser
+					chai.request(server)
+						.post('/api/login')
+						.send(otherRequest)
+						.end((err, res) => {
+							expect(res.status).to.equal(200);
+							expect(res.body).to.have.property('success')
+							expect(res.body).to.have.property('token')
+							expect(res.body).to.have.property('user_id')
+							lUser.token = res.body.token
+							done();
+						})
+				})
+			}).end();
 	})
-	describe('Update group info', function(){
-		it('Exists', function(done){
-
-		})
-		it('Rejects content-free requests', function(done){
-
-		})
-		it('Rejects requests that only have some valid parameters', function(done){
-
-		})
-		it('Rejects valid requests from users that are not logged in', function(done){
-
-		})
-		it('Does not throw 502s or 500s', function(done){
-			
-		})
+	afterEach(function(done){ // Ensure that the return code isn't any of the following
+		expect(retcode).to.not.equal(404);
+		expect(retcode).to.not.equal(500);
+		expect(retcode).to.not.equal(502);
+		done()
 	})
-	describe('Update user', function(){
-		it('Exists', function(done){
-
-		})
-		it('Rejects content-free requests', function(done){
-
-		})
-		it('Rejects requests that only have some valid parameters', function(done){
-
-		})
-		it('Rejects valid requests from users that are not logged in', function(done){
-
-		})
-		it('Does not throw 502s or 500s', function(done){
-			
-		})
-	})
-	describe('Update group debt', function(){
-		it('Exists', function(done){
-
-		})
-		it('Rejects content-free requests', function(done){
-
-		})
-		it('Rejects requests that only have some valid parameters', function(done){
-
-		})
-		it('Rejects valid requests from users that are not logged in', function(done){
-
-		})
-		it('Does not throw 502s or 500s', function(done){
-			
-		})
-	})
-	describe('Update personal debt', function(){
-		it('Exists', function(done){
-
-		})
-		it('Rejects content-free requests', function(done){
-
-		})
-		it('Rejects requests that only have some valid parameters', function(done){
-
-		})
-		it('Rejects valid requests from users that are not logged in', function(done){
-
-		})
-		it('Does not throw 502s or 500s', function(done){
-			
-		})
-	})
-	describe('Update grocery item', function(){
-		it('Exists', function(done){
-
-		})
-		it('Rejects content-free requests', function(done){
-
-		})
-		it('Rejects requests that only have some valid parameters', function(done){
-
-		})
-		it('Rejects valid requests from users that are not logged in', function(done){
-
-		})
-		it('Does not throw 502s or 500s', function(done){
-			
-		})
-	})
-	describe('Update chore item', function(){
-		it('Exists', function(done){
-
-		})
-		it('Rejects content-free requests', function(done){
-
-		})
-		it('Rejects requests that only have some valid parameters', function(done){
-
-		})
-		it('Rejects valid requests from users that are not logged in', function(done){
-
-		})
-		it('Does not throw 502s or 500s', function(done){
-			
-		})
-	})
-	describe('Update rent info', function(){
-		it('Exists', function(done){
-
-		})
-		it('Rejects content-free requests', function(done){
-
-		})
-		it('Rejects requests that only have some valid parameters', function(done){
-
-		})
-		it('Rejects valid requests from users that are not logged in', function(done){
-
-		})
-		it('Does not throw 502s or 500s', function(done){
-			
-		})
+	endpoints.forEach(function(endpoint){
+		describe(endpoint.name, function(){
+			it('Rejects content-free requests', function(done){
+				chai.request(app)
+					.post(endpoint.uri)
+					.end((err, res) => {
+						retcode = res.status
+						expect(res.status).to.equal(400)
+						done()
+					})
+			})
+			it('Rejects requests that only have some valid parameters', function(done){
+				endpoint.badQuery.token = dummyUser.token
+				chai.request(app)
+					.post(endpoint.uri)
+					.send(endpoint.badQuery)
+					.end((err, res) => {
+						retcode = res.status
+						expect(res.status).to.equal(400)
+						done()
+					})
+			})
+			it('Rejects valid requests from users that are not logged in', function(done){
+				chai.request(app)
+					.post(endpoint.uri)
+					.send(endpoint.goodQuery)
+					.end((err, res) => {
+						retcode = res.status
+						expect(res.status).to.equal(401)
+						done()
+					})
+			})
+			it('Rejects valid requests from users that are not in that group', function(done){
+				endpoint.goodQuery.token = dummyUser.token
+				chai.request(app)
+					.post(endpoint.uri)
+					.send(endpoint.goodQuery)
+					.end((err, res) => {
+						retcode = res.status
+						expect(res.status).to.equal(401)
+						done()
+					})
+			})
+			it ('Accepts valid requests', function(done){
+				endpoint.goodQuery.token = lUser.token
+				chai.request(app)
+					.post(endpoint.uri)
+					.send(endpoint.goodQuery)
+					.end((err, res) => {
+						retcode = res.status
+						expect(res.status).to.equal(200)
+						done()
+					})
+			})
+		}
 	})
 })
