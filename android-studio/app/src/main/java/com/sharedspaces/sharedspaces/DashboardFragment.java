07985793380bfad29a913b578e1983d39435680d
choybm <32180312+choybm@users.noreply.github.com>
2017-12-12 16:38:09 -0700

    Fixes, layout changes, added logout button

--- android-studio/app/src/main/java/com/sharedspaces/sharedspaces/DashboardFragment.java
+++ android-studio/app/src/main/java/com/sharedspaces/sharedspaces/DashboardFragment.java
@@ -4,51 +4,97 @@ import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.Toast;
 
 //TODO: implement functionable buttons with working activities, implement "add more" button
 
-public class DashboardFragment extends Fragment implements View.OnClickListener{
+public class DashboardFragment extends Fragment implements View.OnClickListener {
 
-    private Toolbar mToolbar;
+    GridLayout mainGrid;
+    GridLayout mainGrid2;
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         //return inflater.inflate(R.layout.dashboard_fragment, container, false);
 
         View v = inflater.inflate(R.layout.dashboard_fragment, container, false);
+        /*mainGrid = (GridLayout) v.findViewById(R.id.mainGrid);
 
+        setSingleEvent(mainGrid);
+        setSingleEvent(mainGrid2);*/
         Button b1 = (Button) v.findViewById(R.id.button1);
         Button b2 = (Button) v.findViewById(R.id.button2);
+        Button b3 = (Button) v.findViewById(R.id.button3);
+        Button b4 = (Button) v.findViewById(R.id.button4);
         b1.setOnClickListener(this);
         b2.setOnClickListener(this);
+        b3.setOnClickListener(this);
+        b4.setOnClickListener(this);
         return v;
 
     }
 
-        @Override
-        public void onClick(View v) {
-            switch (v.getId()) {
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
 
-                case R.id.button1:
+            //case R.id.button1:
 
-                    Intent i = new Intent(getActivity(),MainActivity.class);
+                /*Intent intent1 = new Intent(getActivity(), .class);
+                startActivity(intent1);
+
+                break;*/
+
+            case R.id.button2:
+
+                Intent intent2 = new Intent(getActivity(),GroceryList.class);
+                startActivity(intent2);
+
+                break;
+
+            //case R.id.button3:
+
+                /*Intent intent3 = new Intent(getActivity(),.class);
+                startActivity(intent3);
+
+                break;*/
+
+            case R.id.button4:
+
+                Intent intent4 = new Intent(getActivity(),MainActivity.class);
+                startActivity(intent4);
+
+                break;
+        }
+    }
+
+}
+
+    /*private void setSingleEvent(GridLayout mainGrid) {
+
+        for (int i = 0; i < mainGrid.getChildCount(); i++) {
+            CardView cardView = (CardView) mainGrid.getChildAt(i);
+            final int finalI = i;
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    Intent i = new Intent(getActivity(), MainActivity.class);
                     startActivity(i);
 
-                    break;
-
-                case R.id.button2:
-
-                    Intent intent = new Intent(getActivity(),GroceryList.class);
-                    startActivity(intent);
-
-                    break;
-            }
+                }
+            });
         }
+    }*/
 
         /*Button grocery = (Button) findViewById(R.id.button2);
         grocery.setOnClickListener(new View.OnClickListener() {
@@ -59,9 +105,9 @@ public class DashboardFragment extends Fragment implements View.OnClickListener{
                 startActivity(intent);
 
             }
-        });*/
+        });
 
 
 
-    }
+    }*/
 
